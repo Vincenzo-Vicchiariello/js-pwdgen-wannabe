da6433e6c969769ac9b90bd670ba2a2fe59d910a
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-03-28 15:40:05 +0200

    Continuo ad avere problemi col layout..

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 
 function pGen() {
-    let nome = prompt("Scrivi il tuo nome qui, su.", "Nome");
+    let nome = prompt("Scrivi il tuo nome qui.", "Nome");
     let cognome = prompt("Scrivi il tuo cognome qui, su.", "Cognome");
-    let colorepref = prompt("Ora ci manca il tuo cognome e possiamo darti la migliore delle password ;)", "Colore");
-    let numero = Math.floor(Math.random() * 1000);
+    let colorepref = prompt("Ci serve il tuo colore preferito, ci siamo quasi!", "Colore");
+    let numero1 = prompt("Dicci un numero, grazie.", "Numero");
+    let numero2 = prompt("Dicci un altro numeretto e ci siamo, grazie.", "Numero");
+    let numero = numero1 / numero2;
     if (nome != null) {
         document.getElementById("demo").innerHTML =
             nome + cognome + colorepref + numero;
